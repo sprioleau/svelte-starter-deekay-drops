7ae8003d91436632fcf768ed02b8c86a68d8ae64
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-01-29 16:04:08 -0500

    🐛 💄 Correct link address, update styling

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,7 +4,7 @@
 
 <footer class="footer">
 	<ul class="footer__list text-muted">
-		<li class="footer__list-item"><a href="https://github.com/sprioleau/deekay-drops-svelte">source</a></li>
+		<li class="footer__list-item"><a href="https://github.com/sprioleau/svelte-starter-deekay-drops"  target="_blank" rel="noreferrer">source</a></li>
 		<li class="footer__list-item">created by <a href="https://github.com/sprioleau">@sprioleau</a></li>
 	</ul>
 </footer>
