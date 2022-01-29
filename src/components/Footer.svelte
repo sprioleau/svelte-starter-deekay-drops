bf9ba5a2e22d2ad2d7a29f6bff4998a3b241017c
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-01-29 16:23:25 -0500

    Update footer with link to companion app

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,9 +2,10 @@
 	import "../styles/components/footer.scss"
 </script>
 
-<footer class="footer">
-	<ul class="footer__list text-muted">
-		<li class="footer__list-item"><a href="https://github.com/sprioleau/svelte-starter-deekay-drops"  target="_blank" rel="noreferrer">source</a></li>
-		<li class="footer__list-item">created by <a href="https://github.com/sprioleau">@sprioleau</a></li>
+<footer class="footer text-muted">
+	<ul class="footer__list">
+		<li class="footer__list-item"><a href="https://github.com/sprioleau/svelte-starter-deekay-drops" target="_blank" rel="noreferrer">source</a></li>
+		<li class="footer__list-item">created by <a href="https://github.com/sprioleau" target="_blank" rel="noreferrer">@sprioleau</a></li>
 	</ul>
+	<p>See the <a href="https://deekay-drops-remix.netlify.app/" target="_blank" rel="noreferrer">Remix version</a></p>
 </footer>
