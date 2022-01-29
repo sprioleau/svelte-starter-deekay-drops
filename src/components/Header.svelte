e1db8ccff205b1ebb2c968b637f9b62068756327
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-01-29 16:33:32 -0500

    Update header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -8,9 +8,9 @@
 		<img src="/deekay-logo.png" alt="DeeKay Logo" class="nav__logo" width={imageWidth} height={imageWidth} />
 	</a>
 	<a href="/" class="no-decor">
-		<h1 class="nav__title">Deekay Drops</h1>
+		<h1 class="nav__title">DeeKay Drops</h1>
 	</a>
 	<p class="text-muted align-center">
-		A small Svelte starter app by <a href="https://www.sprioleau.dev/">S. Prioleau</a>
+		A small Svelte starter app by <a href="https://www.sprioleau.dev/">S. Prioleau</a>. <br /> Logo and all GIFs by <a href="https://deekaykwon.com/">DeeKay Kwon</a>.
 	</p>
 </nav>
